66a0939fceed04670541502b294e5c1f17272736
Akul <akuldpatel16@gmail.com>
2023-02-03 13:34:07 -0500

    third

--- Assignment1/Controllers/HostingCostController.cs
+++ Assignment1/Controllers/HostingCostController.cs
@@ -9,6 +9,12 @@ namespace Assignment1.Controllers
 {
     public class HostingCostController : ApiController
     {
+        /// <summary>
+        /// You are charging your client $5.50 / FN (fortnight = 14 days) for web hosting and maintenance,plus an additional 13% HST. The input {id} represents the number of days which has elapsedsince the beginning of the hosting. Output 3 strings which describe the total hosting cost
+        /// </summary>
+        /// <param name="id">number of days</param>
+        /// <returns>output three strings describing total after taxes</returns>
+        /// shows error when days are 0 and doubts in fortnights count
         public string Get(int id)
         {
             var FN = id / 14;
