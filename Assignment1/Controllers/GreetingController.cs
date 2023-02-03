d3ba9f76c4a4d9522616962526abd7b03f5abe7f
Akul <akuldpatel16@gmail.com>
2023-02-02 22:01:54 -0500

    Commit

---
+++ Assignment1/Controllers/GreetingController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1.Controllers
+{
+    public class GreetingController : ApiController
+    {
+
+        // GET api/Greeting/id
+        public string Get(int id)
+        {
+            return "Greeting to" + id +  "people!" ;
+        }
+
+        public string Post()
+        {
+            return "Hello World!";
+        }
+
+
+    }
+
+}
---
+++ Assignment1/Controllers/HostingCostController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1.Controllers
+{
+    public class HostingCostController : ApiController
+    {
+        public string Get(int id)
+        {
+            var FN = id / 14;
+            var result = FN * 5.50;
+            var HST = result * 0.13;
+            var total = result + HST;
+
+            return "ans" + total;
+        }
+    }
+}
