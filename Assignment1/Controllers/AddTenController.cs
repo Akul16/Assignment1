f8a79583c10adc1a59ed1df3dd169ebe90eff93b
Akul <akuldpatel16@gmail.com>
2023-02-02 22:24:47 -0500

    second

--- Assignment1/Controllers/AddTenController.cs
+++ Assignment1/Controllers/AddTenController.cs
@@ -7,10 +7,23 @@ using System.Web.Http;
 
 namespace Assignment1.Controllers
 {
+   
     public class AddTenController : ApiController
     {
+        /// <summary>
+        /// To return 10 more than the user input id 
+        /// </summary>
+        /// <param name="id">your input integer</param>
+        /// <returns>
+        /// adds 10 to user input
+        /// </returns>
+        /// <example>
+        /// GET: api/AddTen/21 -> 31
+        /// GET: api/AddTen/0 -> 10
+        /// GET: api/AddTen/-9 -> 1
+        /// </example>
 
-        // GET api/AddTen/id
+        
         public int Get(int id)
         {
             return (id + 10);
